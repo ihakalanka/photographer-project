3620e9c3aaf6f2bff0963b11e1e3a246de73cca5
TGimhana <thilinagimhana1997@gmail.com>
2022-08-04 21:57:24 +0530

    second space

--- photographer-webapp-frontend/src/pages/about/about.js
+++ photographer-webapp-frontend/src/pages/about/about.js
@@ -25,7 +25,7 @@ export default function About() {
             {/* end about section */}
             {/* info section */}
             <div>
-                <Footer />
+                 <Footer />
             </div>
         </div>
     )
